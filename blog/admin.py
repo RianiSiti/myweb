1f318b47ceadfcc999bfb7cd1575fd6338bab246
RianiSiti <rianisitinuraeni12@gmail.com>
2019-02-21 23:52:40 -0800

    update & new files

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Artikel
+ 
+#register your models here.
+admin.site.register(Artikel)
+
